cac7b2057290ba23aca7ff2807630de515dff338
Nikita-Hunter <86223001+Nikita-Hunter@users.noreply.github.com>
2021-12-04 12:58:52 +0400

    make header

--- src/js/vendor/menu-button.js
+++ src/js/vendor/menu-button.js
@@ -1,14 +1,17 @@
 const menuBtn = document.getElementById('menu-toggle')
 let menuOpen = false;
+const navMenu = document.getElementById('nav-menu');
 menuBtn.addEventListener('click', () => {
     if(!menuOpen)
     {
         menuBtn.classList.add('open');
+        navMenu.classList.add('open-menu')
         menuOpen = true;
     }
     else
     {
         menuBtn.classList.remove('open');
+        navMenu.classList.remove('open-menu')
         menuOpen = false;
     }
 })
